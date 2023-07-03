d6feeeca633407453bca8a4a3cc45204bf9e356e
unknown <rukhlya1999@mail.ru>
2023-07-03 20:44:31 +0300

    Enhanced bot

--- Bot_with fuction.py
+++ Bot_with fuction.py
@@ -3,7 +3,35 @@ from telebot import types
 import webbrowser
 bot = telebot.TeleBot("6360322093:AAGRgi8gYtIOXhUrK3Csx2RM4UE1pgVsafA")
 
+@bot.message_handler(content_types=["text"])
+def on_click(message):
+    if message.text == "Посетить веб сайт":
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Перейти на веб сайт", url="https://vk.com/audios184956553"))
+        bot.send_message(message.chat.id,"Прошу на сайт",reply_markup=markup)
+    elif message.text == "Удалить фото":
+        bot.send_message(message.chat.id, "Deleted")
+    elif message.text == "Изменить текст":
+        bot.send_message(message.chat.id, "Edited")
 
+
+@bot.message_handler(content_types=["photo"])
+def photo(message):
+    markup=types.InlineKeyboardMarkup()
+    btn1=types.InlineKeyboardButton("Прошу перейдите на сайт",url="https://vk.com/audios184956553")
+    markup.row(btn1)
+    btn2=types.InlineKeyboardButton("Удалить фото",callback_data="delete")
+    btn3=types.InlineKeyboardButton("Изменить текст",callback_data="edit")
+    markup.row(btn2,btn3)
+    bot.reply_to(message,"Ваааууу какое крутое фото!!!!",reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback:True)
+def callback_reply(callback):
+    if callback.data=="delete":
+        bot.delete_message(callback.message.chat.id,callback.message.message_id-2)#Удаляем фотку предыдущую
+    elif callback.data=="edit":
+        bot.edit_message_text("Измененный текст",callback.message.chat.id,callback.message.message_id)
+    
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = types.ReplyKeyboardMarkup()
@@ -12,22 +40,9 @@ def start(message):
     markup.row(btn1, btn2)
     btn3 = types.KeyboardButton("Изменить текст")
     markup.row(btn3)
-    photo = open("E:\\Пашок.jpg", "rb")
-    bot.send_photo(message.chat.id, photo, reply_markup=markup)
-    bot.register_next_step_handler(message, on_click)
+    bot.send_message(message.chat.id, f"Привет {message.from_user.first_name}", reply_markup=markup)
 
 
-def on_click(message):
-    if message.text == "Посетить веб сайт":
-        markup = types.InlineKeyboardMarkup()
-        website = markup.add(types.InlineKeyboardButton(
-            "Посетить веб сайт", url="https://yandex.ru/search/?text=%D0%9A%D0%B8%D0%BB%D0%BB%D0%B8%D0%B0%D0%BD+%D0%9C%D0%B5%D1%80%D1%84%D0%B8&lr=10754&clid=2411726&noreask=1&ento=0oCgpydXcxNDI5NDg1EhJydXcyMDQzOTc6Y3VzdG9tOjAYAnol0JrQuNC90L7QsNC60YLRkdGA0Ysg0JjRgNC70LDQvdC00LjQuFvVfX0"))
-        markup.row(website)
-        bot.send_message(message.chat.id, website)
-    elif message.text == "Удалить фото":
-        bot.send_message(message.chat.id, "Deleted")
-    elif message.text == "Изменить текст":
-        bot.send_message(message.chat.id, "Edited")
 
 
 bot.polling(none_stop=True)
--- Telegram Bot python.py
+++ Telegram Bot python.py
@@ -2,45 +2,56 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot("6218663006:AAGGboF-ByLwUBn0MUrTNv5kiYrAqzwGA34")
-
-
-@bot.message_handler(commands=["start"])
-def buttons(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    photo = types.KeyboardButton("Получить открытку")
-    audio = types.KeyboardButton("Послушать музыку")
-    video = types.KeyboardButton("Посмотреть видос")
-    markup.add(audio, photo, video)
-    bot.send_message(
-        message.chat.id, f"Привет {message.from_user.first_name}, я тестовый бот от Макса. Прошу выбрать нажатие кнопки", reply_markup=markup)
-
-
+name=" "
+surname=" "
+age=0
 @bot.message_handler(content_types=["text"])
-def reply(message):
-
-    if message.text == "Посмотреть видос":
-        video = open("E:\\Для бота.mp4", "rb")
-        bot.send_video(
-            message.chat.id, video)
-
-    elif message.text == "Получить открытку":
-        photo = open("E:\\Пашок.jpg", "rb")
-        bot.send_photo(
-            message.chat.id, photo)
-
-    elif message.text == "Послушать музыку":
-        audio = open("E:\\Инста.mp3", "rb")
-        bot.send_audio(message.chat.id, audio)
-
+def name(message):
+    
+    if message.text=="/start":
+        bot.send_message(message.chat.id,"И так пожалуй начнем знакомство.Как Вас зовут?")
+        bot.register_next_step_handler(message,get_name)
     else:
-        while True:
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button_zaeb = types.KeyboardButton(
-                "Нажми,чтобы все это закончилось")
-            markup.add(button_zaeb)
-            bot.send_message(message.chat.id, "Hello", reply_markup=markup)
-            if message.text == "Нажми,чтобы все это закончилось":
-                break
+        bot.send_message(message.chat.id,"Введите еще раз правильную команду")
+def get_name(message):
+    global name
+    name=message.text
+    bot.send_message(message.chat.id,"Введите свою фамилию")
+    bot.register_next_step_handler(message,get_surname)
+def get_surname(message):
+    global surname
+    surname=message.text
+    bot.send_message(message.chat.id,"Введите свой возраст")
+    bot.register_next_step_handler(message,get_age)
+def get_age(message):
+    global age
+    while age==0:
+        try:
+            age=int(message.text)
+        except Exception:
+            bot.send_message(message.chat.id,"Цифрами пожалуйста")
+    markup=types.InlineKeyboardMarkup()
+    markup_yes=types.InlineKeyboardButton("Да",callback_data="yes")
+    markup_no=types.InlineKeyboardButton("Нет",callback_data="no")
+    markup.add(markup_yes,markup_no)
+    question="Тебе "+str(age)+" лет, зовут тебя "+name+" "+f"{ surname} ?"
+    bot.send_message(message.chat.id,question,reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda callback:True)
+def state(callback):
+    if callback.data =="yes":
+        bot.edit_message_text("Запомню :) ",callback.message.chat.id,callback.message.message_id)
+    elif callback.data=="no":
+        bot.edit_message_text("Введите заново информацию!!",callback.message.chat.id,callback.message.message_id)
+
+
+    # elif callback.text=="no":
+
+
+    
+
+
 
 
 bot.polling(none_stop=True)
