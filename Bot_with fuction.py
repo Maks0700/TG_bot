e82729948587dcf5e777c3844644019e126c5201
unknown <rukhlya1999@mail.ru>
2023-07-05 18:20:09 +0300

    Database

--- Bot_with fuction.py
+++ Bot_with fuction.py
@@ -1,48 +1,49 @@
 import telebot
 from telebot import types
 import webbrowser
+import sqlite3
+
 bot = telebot.TeleBot("6360322093:AAGRgi8gYtIOXhUrK3Csx2RM4UE1pgVsafA")
-
-@bot.message_handler(content_types=["text"])
-def on_click(message):
-    if message.text == "Посетить веб сайт":
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("Перейти на веб сайт", url="https://vk.com/audios184956553"))
-        bot.send_message(message.chat.id,"Прошу на сайт",reply_markup=markup)
-    elif message.text == "Удалить фото":
-        bot.send_message(message.chat.id, "Deleted")
-    elif message.text == "Изменить текст":
-        bot.send_message(message.chat.id, "Edited")
+name=" "
 
 
-@bot.message_handler(content_types=["photo"])
-def photo(message):
-    markup=types.InlineKeyboardMarkup()
-    btn1=types.InlineKeyboardButton("Прошу перейдите на сайт",url="https://vk.com/audios184956553")
-    markup.row(btn1)
-    btn2=types.InlineKeyboardButton("Удалить фото",callback_data="delete")
-    btn3=types.InlineKeyboardButton("Изменить текст",callback_data="edit")
-    markup.row(btn2,btn3)
-    bot.reply_to(message,"Ваааууу какое крутое фото!!!!",reply_markup=markup)
-
-@bot.callback_query_handler(func=lambda callback:True)
-def callback_reply(callback):
-    if callback.data=="delete":
-        bot.delete_message(callback.message.chat.id,callback.message.message_id-2)#Удаляем фотку предыдущую
-    elif callback.data=="edit":
-        bot.edit_message_text("Измененный текст",callback.message.chat.id,callback.message.message_id)
-    
 @bot.message_handler(commands=["start"])
 def start(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton("Посетить веб сайт")
-    btn2 = types.KeyboardButton("Удалить фото")
-    markup.row(btn1, btn2)
-    btn3 = types.KeyboardButton("Изменить текст")
-    markup.row(btn3)
-    bot.send_message(message.chat.id, f"Привет {message.from_user.first_name}", reply_markup=markup)
+    conn=sqlite3.connect('example.sql')
+    cur=conn.cursor()
 
+    cur.execute("""CREATE TABLE IF NOT EXISTS cars (
+        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        model TEXT,
+        price INTEGER
+    )""")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+    bot.send_message(message.chat.id,"Привет, сейчас мы тебя зарегистрируем!Введите свое имя")
+    bot.register_callback_query_handler(message,user_name)
+   
+def user_name(message):
+    global name
+    name=message.text.strip()
+    bot.send_message(message.chat.id,"Введите пароль:")
+    bot.register_next_step_handler(message,user_pass)
+def user_pass(message):
+    password=message.text.strip()
+    bot.send_message(message.chat.id,)
+    conn=sqlite3.connect("example.sql")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO users(name,pass) VALUES('%s','%s')"%(name,password))
+    conn.commit()
+    cur.close()
+    conn.close()
+    markup=types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Список пользователей",callback_data="users"))
+    bot.send_message(message.chat.id,"Пользователь зарегистрирован!!",reply_markup=markup)
 
 
 
 bot.polling(none_stop=True)
+
